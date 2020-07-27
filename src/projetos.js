f617aa41b0ab1049e129a91692902040b644b7f5
Cleiton <cleiton.rce@gmail.com>
2020-07-26 23:31:53 -0300

    update_cleiton_20200725_174439

--- src/projetos.js
+++ src/projetos.js
@@ -130,6 +130,104 @@ module.exports = {
         return 'update_' + process.env.USER_NAME + '_'  + now.getFullYear() + f.zeroLeft(now.getMonth()+1,2) + f.zeroLeft(now.getDate(),2) + '_' + f.zeroLeft(now.getHours(),2) + f.zeroLeft(now.getMinutes(),2) + f.zeroLeft(now.getSeconds(),2)
     },
 
+    ///// NEWVERSION - GERA A NOVA VERSÃO E ATUALIZADO O ARQUIVO version-app.js /////
+    newVersion(projetoSelected,branch,callback){
+        const lineReader = require('line-reader');
+
+        var newFileVersion = []
+        var gateHistoric = false
+        var oldVersion = ''
+        var newVersion = ''
+        var save = false
+
+        s.cd(process.env.WORKSPACE_PATH + projetoSelected)
+        lineReader.eachLine('version-app.js', (line, last) => {
+            if(gateHistoric===true && f.strlen(newVersion)>=5 && f.strlen(newVersion)<=11 && f.strlen(line)>0 && save===false){
+                gateHistoric = false
+                save = true
+
+                const now = new Date()
+                var newHistoric = f.zeroLeft(now.getDate(),2) + '/' + f.zeroLeft(now.getMonth()+1,2) + '/' + now.getFullYear() + ' AS ' + f.zeroLeft(now.getHours(),2) + ':' + f.zeroLeft(now.getMinutes(),2) + ':' + f.zeroLeft(now.getSeconds(),2) + ' - ' + newVersion + ' - BRANCH: ' + branch
+
+                newFileVersion.push(newHistoric)
+                newFileVersion.push(line)
+            }else if(line.indexOf('const version')!=-1){
+                var quotation = ''
+                if(line.indexOf('\'')!=-1){
+                    quotation = '\''
+                }else if(line.indexOf('"')!=-1){
+                    quotation = '"'
+                }
+                if(f.strlen(quotation)==0){
+                    f.banner(['Formato da versão inválido!(1)'],'left',false,true,true)
+                }else{
+                    var posA = line.indexOf(quotation)
+                    var subString = line.substring(posA + 1)
+                    var posB = subString.indexOf(quotation)
+                    var versionString = subString.substring(0,posB)
+                    if(f.strlen(versionString)<5 || f.strlen(versionString)>11){
+                        f.banner(['Formato da versão inválido!(2)'],'left',false,true,true)
+                    }else{
+                        var version = versionString.split('.')
+                        if(f.count(version)!=3){
+                            f.banner(['Formato da versão inválido!(3)'],'left',false,true,true)
+                        }else{
+                            oldVersion = versionString
+                            Object.keys(version).map(k => {
+                                version[k] = version[k] * 1
+                            })
+                            if(version[2]==9){
+                                version[2] = 0
+                                if(version[1]==9){
+                                    version[1] = 0
+                                    version[0] = version[0] + 1
+                                }else{
+                                    version[1] = version[1] + 1
+                                }
+                            }else{
+                                version[2] = version[2] + 1
+                            }
+                            newVersion = version[0] + '.' + version[1] + '.' + version[2]
+                            newFileVersion.push('const version = \'' + newVersion + '\'')
+                        }
+                    }
+                    
+                }
+            }else if(line.toLowerCase().indexOf('hist')!=-1){
+                newFileVersion.push(line)
+                gateHistoric = true
+            }else{
+                newFileVersion.push(line)
+            }
+            if(line.indexOf(branch)!=-1){
+                save = 1
+            }
+        },() => {
+            if(save===false){
+                f.banner(['Houve uma falha ao tentar gerar a nova versão!'],'left',false,true,true)
+                callback()
+            }else if(save==1 && save!==true){
+                f.banner(['Mantendo a mesma versão ' + oldVersion + ' gerada anteriormente!'],'left',false,true,true)
+                callback()
+            }else{
+                var content = ''
+                newFileVersion.map(line => {
+                    content += line + '\n'
+                })
+                const data = new Uint8Array(Buffer.from(content));
+                fs.writeFile('version-app.js', data, (error) => {
+                    if(error){
+                        f.banner(['Houve uma falha: ' + error + '!'],'left',false,true,true)
+                        callback()
+                    }else{
+                        f.banner(['Nova versão ' + newVersion + ' gerada com sucesso!'],'left',false,true,true)
+                        callback()
+                    }
+                });
+            }
+        })
+    },
+
     ///// PULLFULL - EXECUTA O PULL COMPLETO (BRANCH,CHECKOUT,PULL)
     pullFull(projetoSelected){
         this.head(projetoSelected)
@@ -229,77 +327,79 @@ module.exports = {
                 if(answer=='1'){
                     const branch = this.getBranch(projetoSelected)
                     this.head(projetoSelected)
-                    if(s.exec('git add .').code !== 0) {
-                        f.question([
-                            'HOUVE UMA FALHA AO TENTAR EXECUTAR O ADD!',
-                            '<br>',
-                            '0 - VOLTAR'
-                        ],(answer) => {
-                            const projetos = require('./projetos')
-                            projetos.exec(projetoSelected)
-                        })
-                    }else{
-                        f.question([
-                            '<br>',
-                            'DESEJA PROSSEGUIR COM O COMMIT?',
-                            'COMMIT: ' + branch,
-                            '<br>',
-                            '1 - SIM | 0 - NÃO',
-                            '<br>'
-                        ],(answer) => {
-                            if(answer=='1'){
-                                this.head(projetoSelected)
-                                if(s.exec('git commit -m "' + branch + '"').code !== 0) {
-                                    f.question([
-                                        'HOUVE UMA FALHA AO TENTAR EXECUTAR O COMMIT!',
-                                        '<br>',
-                                        '0 - VOLTAR'
-                                    ],(answer) => {
-                                        const projetos = require('./projetos')
-                                        projetos.exec(projetoSelected)
-                                    })
-                                }else{
-                                    f.question([
-                                        '<br>',
-                                        'DESEJA PROSSEGUIR COM O PUSH?',
-                                        'PUSH BRANCH: ' + branch,
-                                        '<br>',
-                                        '1 - SIM | 0 - NÃO',
-                                        '<br>'
-                                    ],(answer) => {
-                                        if(answer=='1'){
-                                            this.head(projetoSelected)
-                                            if(s.exec('git push -u origin ' + branch).code !== 0) {
-                                                f.question([
-                                                    'HOUVE UMA FALHA AO TENTAR EXECUTAR O PUSH!',
-                                                    '<br>',
-                                                    '0 - VOLTAR'
-                                                ],(answer) => {
-                                                    const projetos = require('./projetos')
-                                                    projetos.exec(projetoSelected)
-                                                })
-                                            }else{
-                                                f.question([
-                                                    'PUSH REALIZADO COM SUCESSO!',
-                                                    '<br>',
-                                                    '0 - VOLTAR'
-                                                ],(answer) => {
-                                                    const projetos = require('./projetos')
-                                                    projetos.exec(projetoSelected)
-                                                },false,true,true)
-                                            }
-                                        }else{
+                    this.newVersion(projetoSelected,branch,() => {
+                        if(s.exec('git add .').code !== 0) {
+                            f.question([
+                                'HOUVE UMA FALHA AO TENTAR EXECUTAR O ADD!',
+                                '<br>',
+                                '0 - VOLTAR'
+                            ],(answer) => {
+                                const projetos = require('./projetos')
+                                projetos.exec(projetoSelected)
+                            })
+                        }else{
+                            f.question([
+                                '<br>',
+                                'DESEJA PROSSEGUIR COM O COMMIT?',
+                                'COMMIT: ' + branch,
+                                '<br>',
+                                '1 - SIM | 0 - NÃO',
+                                '<br>'
+                            ],(answer) => {
+                                if(answer=='1'){
+                                    this.head(projetoSelected)
+                                    if(s.exec('git commit -m "' + branch + '"').code !== 0) {
+                                        f.question([
+                                            'HOUVE UMA FALHA AO TENTAR EXECUTAR O COMMIT!',
+                                            '<br>',
+                                            '0 - VOLTAR'
+                                        ],(answer) => {
                                             const projetos = require('./projetos')
                                             projetos.exec(projetoSelected)
-                                        }
-                                    },false,true,true)
+                                        })
+                                    }else{
+                                        f.question([
+                                            '<br>',
+                                            'DESEJA PROSSEGUIR COM O PUSH?',
+                                            'PUSH BRANCH: ' + branch,
+                                            '<br>',
+                                            '1 - SIM | 0 - NÃO',
+                                            '<br>'
+                                        ],(answer) => {
+                                            if(answer=='1'){
+                                                this.head(projetoSelected)
+                                                if(s.exec('git push -u origin ' + branch).code !== 0) {
+                                                    f.question([
+                                                        'HOUVE UMA FALHA AO TENTAR EXECUTAR O PUSH!',
+                                                        '<br>',
+                                                        '0 - VOLTAR'
+                                                    ],(answer) => {
+                                                        const projetos = require('./projetos')
+                                                        projetos.exec(projetoSelected)
+                                                    })
+                                                }else{
+                                                    f.question([
+                                                        'PUSH REALIZADO COM SUCESSO!',
+                                                        '<br>',
+                                                        '0 - VOLTAR'
+                                                    ],(answer) => {
+                                                        const projetos = require('./projetos')
+                                                        projetos.exec(projetoSelected)
+                                                    },false,true,true)
+                                                }
+                                            }else{
+                                                const projetos = require('./projetos')
+                                                projetos.exec(projetoSelected)
+                                            }
+                                        },false,true,true)
+                                    }
+                                }else{
+                                    const projetos = require('./projetos')
+                                    projetos.exec(projetoSelected) 
                                 }
-                            }else{
-                                const projetos = require('./projetos')
-                                projetos.exec(projetoSelected) 
-                            }
-                        },false,true,true)
-                    }
+                            },false,true,true)
+                        }
+                    })
                 }else{
                     const projetos = require('./projetos')
                     projetos.exec(projetoSelected)
